8ce7cabcc70ebd26713ddf4e29e5dd478388ab90
Mathis Welker <166881564+mathiswelker@users.noreply.github.com>
2025-11-18 22:33:13 +0100

    Modify API response to include user message

--- api/test/index.js
+++ api/test/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
-    context.log("API called");
-    return {
-        status: 200,
-        body: { message: "Hello from /api/test" }
-    };
+  const userMessage = req.body?.message || "Hello";
+  return {
+    status: 200,
+    body: { message: `You said: "${userMessage}"` },
+  };
 };
