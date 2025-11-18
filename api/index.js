4b194c7c00df439fcff109396a2bfcbc110a11e6
Mathis Welker <166881564+mathiswelker@users.noreply.github.com>
2025-11-19 00:05:22 +0100

    Delete api/index.js

--- api/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  const userMessage = req.body?.message || "Hello";
-  return {
-    status: 200,
-    body: { message: `You said: "${userMessage}"` },
-  };
-};
